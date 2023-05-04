a25e6ef2b9849e3218e3094f07e3a9874a490e2e
annzhzho@gmail.com <annzhzho@gmail.com>
2023-05-04 19:32:21 +0300

    add hw

---
+++ index.js
@@ -0,0 +1,42 @@
+let cleaner = 40000;
+let teacher = 22000;
+console.log (cleaner + teacher);
+
+let city = 'Владивосток';
+let cityNumber = '2000';
+console.log (city + " " + cityNumber);
+
+let messanger = 128;
+photo = 254;
+vk = 201;
+youtube = 201;
+console.log (messanger + photo + vk + youtube);
+
+let number = 5;
+let degree = 2;
+console.log (number**degree);
+
+let quote = "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log (quote);
+
+
+const greetings = "Привет";
+const name = "Кот";
+console.log (`${greetings}, ${name}`);
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+
+time -= 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+let farenheit = 451;
+let celsius = (farenheit - 32) / 1.8;
+console.log(`${farenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсиюю`);
